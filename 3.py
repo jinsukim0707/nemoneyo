60311a18e939848543d57e56e9b82de0361ec973
jinsukim0707 <75301539+jinsukim0707@users.noreply.github.com>
2021-01-06 21:56:49 +0900

    Update 3.py

--- 3.py
+++ 3.py
@@ -5,3 +5,13 @@ if balance >= withdrawl:
     print(str(withdrawl)+"원을 출금합니다.")
     balance = balance - withdrawl 
 print("잔고: " + str(balance))
+
+def func(x,y):
+return 2*x+y**2
+
+print(type(func))
+print(func(3,4), end='\n\n')
+
+f=lambda x,y: x**2+2*y
+print(type(f))
+print(f(3,4))
